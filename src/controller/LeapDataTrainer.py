1aba4d09e2c1e965706e8295d411269acb910a4c
Joshua Pocsidio <joshua.pocsidio@gmail.com>
2020-09-24 23:31:51 +0800

    changing from 4 to 12 core processing

--- src/controller/LeapDataTrainer.py
+++ src/controller/LeapDataTrainer.py
@@ -116,7 +116,7 @@ class DT_Trainer(Trainer):
         }
 
         # Initialize hyper parameter tuning grid search
-        grid_classifier = GridSearchCV(classifier, grid_parameters, n_jobs=4, cv=5)
+        grid_classifier = GridSearchCV(classifier, grid_parameters, n_jobs=12, cv=5)
         # Fit the model
         grid_classifier.fit(X_train, y_train)
 
@@ -176,7 +176,7 @@ class NN_Trainer(Trainer):
             'solver': ['adam', 'sgd']
         }
         # Initialize hyper parameter tuning grid search
-        grid_classifier = GridSearchCV(classifier, grid_parameters, n_jobs=4, cv=5)
+        grid_classifier = GridSearchCV(classifier, grid_parameters, n_jobs=12, cv=5)
         # Fit the model
         grid_classifier.fit(X_train, y_train)
 
@@ -229,7 +229,7 @@ class SVM_Trainer(Trainer):
         }
 
         # Initialize hyper parameter tuning grid search
-        grid_classifier = GridSearchCV(classifier, grid_parameters, n_jobs=8, cv=5, verbose=True)
+        grid_classifier = GridSearchCV(classifier, grid_parameters, n_jobs=12, cv=5, verbose=True)
         # Fit the model
         grid_classifier.fit(X_train, y_train)
 
